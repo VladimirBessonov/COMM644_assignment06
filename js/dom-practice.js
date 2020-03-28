9c090ef0a293c11118638c8322d91317b46d24ff
vladimirbessonov <2000.besson@gmail.com>
2020-03-28 12:41:50 -0700

    first commit

---
+++ js/dom-practice.js
@@ -0,0 +1,28 @@
+/*eslint-env browser*/
+window.onload = function() {
+
+//STEP 1
+
+document.getElementById("btn1").addEventListener("click", function() {
+    alert('Button "OK" was clicked')
+});
+
+//STEP 2
+
+//STEP 3
+
+//STEP 4
+
+//STEP 5
+
+//STEP 6
+
+//STEP 7
+
+//STEP 8
+
+//STEP 9
+
+//STEP 10
+
+};
