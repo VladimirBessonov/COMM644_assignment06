6f482e0e097fa9c0060fbf911771fd437a452656
vladimirbessonov <2000.besson@gmail.com>
2020-03-30 20:49:07 -0700

    add form validation and Obj to keep the employees

--- js/employee-management.js
+++ js/employee-management.js
@@ -1,4 +1,39 @@
 var EmployeeList = [];
+
+var tableBody = document.getElementsByTagName('tbody')
+
+let observe = (obj, fn) => new Proxy(obj, {
+    set(obj, key, val) {
+        obj[key] = val;
+        fn(obj)
+        // createTableBody(EmployeeList)
+        return true
+        }
+    }
+);
+
+arr = observe(EmployeeList, arr => {
+    // console.log('arr changed! ', arr)
+    createTableBody(arr)
+    console.log('in proxi call')
+});
+
+
+function createTableBody (arr) {
+    for (i =0; i < arr.length; i++) {
+        console.log(arr[i])
+        var tr = document.createElement("tr");
+        for (j =0; j < arr[i].length; j++) {
+            console.log(arr[i][j])
+            var td = document.createElement("td");
+            td.innerText = arr[i][j]
+            tr.appendChild(td);
+        }
+        tableBody[0].appendChild(tr)
+    }
+    console.log(tableBody[0])
+}
+
 $(document).ready(function(){
 
     $('#newEmployee').submit( function (event) {
@@ -16,19 +51,27 @@ $(document).ready(function(){
         if (status.isValid == false) {
 
             if (name.name == "" || title.title == "" || ext.ext == "") {
-
+                $("#name").next().text("Required");
+                $("#title").next().text("Required");
+                $("#ext").next().text("Required");
                 alert('All fields are required')
             }
-
         }
         if (status.isValid == true) {
 
-            EmployeeList.push({name, title, ext})
+            arr.push({name, title, ext})
             $("#name").next().text("");
+            $("#name").val("");
+            $("#name").attr('style', '');
             $("#title").next().text("");
+            $("#title").val("");
+            $("#title").attr('style', '');
             $("#ext").next().text("");
+            $("#ext").val("");
+            $("#ext").attr('style', '');
+            console.log(EmployeeList)
         }
-        console.log(EmployeeList)
+
     })
     $('.employees').click( function (event) {
         const { target } = event;
@@ -45,22 +88,15 @@ $(document).ready(function(){
 
 function verifyField(obj) {
     var id = Object.keys(obj)[0]
-    console.log(obj)
-    console.log(id)
     if (obj[id] == "") {
         $(`#${id}`).next().text("This field is required.");
         $(`#${id}`).css('border-color', 'red')
         this.isValid = false
     } else {
         $(`#${id}`).next().text("");
-        $(`#${id}`).css('border-color', 'green')
+        // $(`#${id}`).css('border-color', 'green')
     }
 
 }
 
 
-
-// TODO:
-/*
-fields in a table are editable, after add table is updates
- */
