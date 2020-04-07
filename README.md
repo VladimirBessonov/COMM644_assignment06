990ac6541c6d7384b39aec857562f5aff88838f2
vladimirbessonov <2000.besson@gmail.com>
2020-04-06 22:32:18 -0700

    changed data structure for arr

--- README.md
+++ README.md
@@ -2,3 +2,16 @@
 Example of reactive app without famous libs 
 - event handlers
 - proxy to observe an object changes 
+
+## Author
+
+- **Vladimir Bessonov** -
+
+## License
+
+This project is licensed under the MIT License - see the [MIT License](https://simple.wikipedia.org/wiki/MIT_License) file for details
+
+## Acknowledgments
+
+- SDCCD (San Diego Community College District)
+- Zak Ruvalcaba
--- js/employee-management.js
+++ js/employee-management.js
@@ -11,8 +11,24 @@ $(document).ready(function(){
             }
         }
     );
+    let observe1 = (obj, fn) => new Proxy(obj, {
+
+            set(obj, key, val) {
+
+                obj[key] = val;
+                fn(obj)
+                return true
+            }
+        }
+    );
 
     arr = observe(EmployeeList, arr => {
+        countEmployees(arr)
+        createTableBody(arr)
+        console.log(arr)
+    });
+    arr1 = observe(EmployeeList, arr => {
+
         countEmployees(arr)
         createTableBody(arr)
         console.log(arr)
@@ -24,33 +40,31 @@ $(document).ready(function(){
         let tableBody = document.getElementById('tb')
         // if (tableBody.childElementCount !==0 ) {
             tableBody.innerHTML = '';
-            for (row of arr) {
-                let tr = document.createElement("tr");
-                for (cell in row) {
-                    let td = document.createElement("td");
-                    let input = document.createElement('input')
-                    input.setAttribute("type", "text");
-                    input.setAttribute('value', `${Object.values(row[cell])}`)
-                    input.disabled = true;
-                    // td.innerText = Object.values(row[cell])
-                    td.appendChild(input)
-                    tr.appendChild(td);
-                }
-                // no idea why createRange cannot creete <td><button>, <td><button><button/><tb/> will produce only <button><button/>
-                let btn1 = document.createRange().createContextualFragment('<button name="Edit" class="btn btn-primary">Edit</button>')
-                let td1 = document.createElement("td")
-                let btn2 = document.createRange().createContextualFragment('<button name="Delete" class="btn btn-danger">Delete</button>')
-                let td2 = document.createElement("td")
-                td1.appendChild(btn1);
-                td2.appendChild(btn2);
-                tr.appendChild(td1);
-                tr.appendChild(td2);
-                tableBody.appendChild(tr)
 
+        arr.forEach( item => {
+            let tr = document.createElement("tr");
+            for ( let key in item) {
+                let td = document.createElement("td");
+                let input = document.createElement('input')
+                input.setAttribute("type", "text");
+                input.setAttribute('value', `${item[key]}`)
+                input.disabled = true;
+                td.appendChild(input)
+                tr.appendChild(td);
             }
-        }
+            let btn1 = document.createRange().createContextualFragment('<button name="Edit" class="btn btn-primary">Edit</button>')
+            let td1 = document.createElement("td")
+            let btn2 = document.createRange().createContextualFragment('<button name="Delete" class="btn btn-danger">Delete</button>')
+            let td2 = document.createElement("td")
+            td1.appendChild(btn1);
+            td2.appendChild(btn2);
+            tr.appendChild(td1);
+            tr.appendChild(td2);
+            tableBody.appendChild(tr)
 
-    // }
+        })
+
+    }
 
  // count number of employees in the list
     function countEmployees (arr) {
@@ -59,33 +73,41 @@ $(document).ready(function(){
     }
 // initial value for the counter of employees and add  employee
     $('h2 span').text('0')
-    arr.push({name: {name: 'Vladimir Bessonov'}, title: {title: 'Software engineer'}, ext: {ext: '123456'}},{name: {name: 'XXXXXXXX'}, title: {title: 'Software engineer'}, ext: {ext: '123456'}})
+    arr.push({name: 'Vladimir Bessonov', title: 'Software engineer', ext: '123456' })
 
 // add new employee to the Table
 
     $('#newEmployee').submit( function (event) {
         event.preventDefault()
-        let status = {isValid: true};
-        let name = {name: $('#name').val().trim()}
-        let title = {title: $('#title').val().trim()}
-        let ext = {ext: $('#ext').val().trim()}
+        // let status = {isValid: true};
+        let status = true;
+        // let name = {name: $('#name').val().trim()}
+        let name = $('#name').val().trim()
+        // let title = {title: $('#title').val().trim()}
+        let title = $('#title').val().trim()
+        // let ext = {ext: $('#ext').val().trim()}
+        let ext = parseInt($('#ext').val().trim())
 // add status to vefiry scope
         let verify = verifyField.bind(status);
 
-        verify(name);
-        verify(title);
-        verify(ext);
+        verify(name, 'name');
+        verify(title, 'title');
+        verify(ext, 'title');
 
-        if (status.isValid == false) {
+        // if (status.isValid == false)
+        if (status == false)
 
-            if (name.name == "" || title.title == "" || ext.ext == "") {
+        {
+            if (name == "" || title == "" || ext == "") {
                 $("#name").next().text("Required");
                 $("#title").next().text("Required");
                 $("#ext").next().text("Required");
                 alert('All fields are required')
             }
         }
-        if (status.isValid == true) {
+        // if (status.isValid == true)
+        if (status == true)
+        {
             console.log({name, title, ext})
             arr.push({name, title, ext})
             $("#name").next().text("");
@@ -140,14 +162,9 @@ $(document).ready(function(){
                     inputs.forEach( el => el.disabled = true)
                     target.textContent = 'Edit'
                     target.name = 'Edit'
-                    arr[index] = {name : {name: inputs[0].value}, title: {title: inputs[1].value}, ext : {ext: inputs[2].value}}
-
+                    arr[index] = {name: inputs[0].value, title: inputs[1].value, ext: inputs[2].value}
                 }
-
-
             }
-
-
         }
         if (target.matches('td')) {
             // console.log('td clicked')
@@ -156,15 +173,15 @@ $(document).ready(function(){
     })
 
 });
-// not good solution: this function complicates the data structure of EmloyeeList, example EmloyeeList = [ { name: {name: 'Vladimir'}, title: {title: 'SW engineer', ext: {ext: 123456}}, {}, ...]
-function verifyField(obj) {
-    var id = Object.keys(obj)[0]
-    if (obj[id] == "") {
-        $(`#${id}`).next().text("This field is required.");
-        $(`#${id}`).css('border-color', 'red')
+
+function verifyField(field, name) {
+
+    if (field == "") {
+        $(`#${name}`).next().text("This field is required.");
+        $(`#${name}`).css('border-color', 'red')
         this.isValid = false
     } else {
-        $(`#${id}`).next().text("");
+        $(`#${name}`).next().text("");
         // $(`#${id}`).css('border-color', 'green')
     }
 
